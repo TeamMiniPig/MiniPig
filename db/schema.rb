30409ef90f57f61569635b7a8efbe0a4760bf48d
pavasich <paul.vasich@gmail.com>
2015-12-08 21:40:55 -0600

    redid some stylings and added an about button when logged in

--- db/schema.rb
+++ db/schema.rb
@@ -13,43 +13,40 @@
 
 ActiveRecord::Schema.define(version: 20151208211950) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "categories", force: :cascade do |t|
-    t.string "category_name"
-    t.string "category_description"
+    t.string "category_name",        limit: 255
+    t.string "category_description", limit: 255
   end
 
   create_table "hoontas", force: :cascade do |t|
-    t.string "hoonta_name"
-    t.string "hoonta_password"
+    t.string "hoonta_name",     limit: 255
+    t.string "hoonta_password", limit: 255
   end
 
   create_table "ideas", force: :cascade do |t|
-    t.string  "idea_name"
-    t.string  "idea_description"
-    t.integer "user_id"
-    t.integer "category_id"
+    t.string  "idea_name",        limit: 255
+    t.string  "idea_description", limit: 255
+    t.integer "user_id",          limit: 4
+    t.integer "category_id",      limit: 4
   end
 
   create_table "rosters", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "hoonta_id"
+    t.integer "user_id",   limit: 4
+    t.integer "hoonta_id", limit: 4
   end
 
   create_table "topics", force: :cascade do |t|
-    t.string  "topic_name"
-    t.string  "deadline"
-    t.integer "hoonta_id"
-    t.integer "category_id"
+    t.string  "topic_name",  limit: 255
+    t.string  "deadline",    limit: 255
+    t.integer "hoonta_id",   limit: 4
+    t.integer "category_id", limit: 4
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name"
-    t.string "display_name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "user_name",       limit: 255
+    t.string "display_name",    limit: 255
+    t.string "email",           limit: 255
+    t.string "password_digest", limit: 255
   end
 
 end
