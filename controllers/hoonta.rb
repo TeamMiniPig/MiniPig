6ed192f5bc4a07095d83617efeecf3cb4f886d6a
Ruth Thatcher <runic77@gmail.com>
2015-12-09 10:58:39 -0600

    button text

--- controllers/hoonta.rb
+++ controllers/hoonta.rb
@@ -42,7 +42,7 @@ class HoontaController < ApplicationController
     hoonta_name = params[:hoonta_name]
 
     if hoonta_name
-      hoonta_name = hoonta_name.downcase
+    #   hoonta_name = hoonta_name.downcase
       hoonta = Hoonta.find_by(hoonta_name: hoonta_name)
 
       if hoonta
@@ -95,5 +95,6 @@ class HoontaController < ApplicationController
       redirect '/hoonta/home'
     end
   end
+
 
 end
