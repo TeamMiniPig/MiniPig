3d1155c51ed8670b2dc65ce98116519dd284b2c1
pavasich <paul.vasich@gmail.com>
2015-12-10 23:43:43 -0600

    goodbye, roster.rb

--- controllers/roster.rb
+++
@@ -1,7 +0,0 @@
-class RosterController < ApplicationController
-  # get '/create' do
-  #   authorized?
-  #   Roster.create(user_id: params[:user_id], hoonta_id: params[:hoonta_id])
-  #   redirect '/hoonta/home'
-  # end
-end
